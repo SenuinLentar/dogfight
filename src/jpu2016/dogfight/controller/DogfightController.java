0e505479fda54fa4b69902249a32c6236b5e78ef
Happocalypse <maxime.neuville@viacesi.fr>
2017-06-01 21:52:24 +0200

    Phase 3
    Phase 3

--- src/jpu2016/dogfight/controller/DogfightController.java
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -1,5 +1,31 @@
 package jpu2016.dogfight.controller;
 
-public class DogfightController {
+public class DogfightController implements IOrderPerformer{
 
+	private int TIME_SLEEP;
+	
+	public DogfightController(IDogfightModel dogfightModel){
+		
+	}
+	
+	public void orderPerform(UserOrder userOrder){
+		
+	}
+	
+	public void player(){
+		
+	}
+	
+	public void setViewSystem(IViewSystem viewSystem){
+		
+	}
+	
+	private void lauchMissile(int player){
+		
+	}
+	
+	private void gameLoop(){
+		
+	}
+	
 }
