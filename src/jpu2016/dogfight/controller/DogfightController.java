1bd1dc35fbc91c9f42add5242fc3e77c1761dd04
Happocalypse <maxime.neuville@viacesi.fr>
2017-06-02 13:41:51 +0200

    Phase 4
    Corrections d'erreur, ajout d'un private Order order pour retirer l'erreur, ajout des commentaire sur les méthode, précision sur d'autres méthodes.

--- src/jpu2016/dogfight/controller/DogfightController.java
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -5,18 +5,21 @@ import jpu2016.dogfight.model.*;
 
 public class DogfightController implements IOrderPerformer{
 
-	private int TIME_SLEEP;
+	private int TIME_SLEEP = 30;
 	
 	public DogfightController(IDogfightModel dogfightModel){
 		
 	}
 	
+	
 	public void orderPerform(UserOrder userOrder){
-		
+		//La méthode orderPerform() récupère les ordre envoyés par la vue et en fonction lance les
+		//méthodes adéquat sur les éléments du modèle (plane.setDirection()). 
 	}
 	
 	public void play(){
-		
+		//affiche un message (via l’interface IViewSystem fournie par la vue) de fin de partie
+		this.gameLoop();
 	}
 	
 	public void setViewSystem(IViewSystem viewSystem){
@@ -30,5 +33,17 @@ public class DogfightController implements IOrderPerformer{
 	private void gameLoop(){
 		
 	}
+	
+	private void isWeaponOnMobile(){
+		//La méthode isWeaponOnMobile() permet de calculer si un missile se trouve sur un mobile. 
+	}
+	
+	public void manageCollision(){
+		//La méthode manageCollision() a la charge de gérer les collisions des missiles (via la
+		//méthode isWeaponOnMobile()), de détruire le missile et de prévenir l’avion en fonction du
+	    //résultat. Pas très compliqué en soi.
+	}
+	
+	
 	
 }
--- src/jpu2016/dogfight/controller/UserOrder.java
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -3,12 +3,12 @@ package jpu2016.dogfight.controller;
 public class UserOrder implements IUserOrder{
 	
 	private int player;
+	private Order order;
 	
-	public UserOrder(){
-		
-	}
 	
 	public UserOrder(int player, Order order){
+		this.order = order;
+		this.player = player;
 		
 	}
 	
