3101062fc9eb41a6fd02fd41f335c54b4f17f012
Happocalypse <maxime.neuville@viacesi.fr>
2017-06-02 13:40:12 +0200

    Phase 4
    Ajout des commentaires sur le main

--- src/jpu2016/dogfight/Dogfight.java
+++ src/jpu2016/dogfight/Dogfight.java
@@ -13,7 +13,12 @@ public class Dogfight {
 		dogfightController.setViewSystem(dogfightView);
 		dogfightController.play();
 
-		
+		//Dans le main, c’est vous qui créerez les avions et les nuages.
+		//L’ordre dans lequel vous le ferez aura une incidence sur le jeu.
+		//En effet, les premiers créés seront sous les autres. Si vous créez les avions avant les nuages, ces derniers cacheront les avions.
+		//Vous pouvez donc tout à fait créer des nuages devant et d’autres derrières les avions.
+		//Les missiles (créés pendant la partie et donc en dernier) seront eux toujours devant.
+		//Si ce n’est pas le cas, c’est que la vue a dans sa boucle d’affichage commencé par la fin et mobiles.
 	}
 
 }
